b09f5eb1b11dae04478657ef16100c7df29b0b96
AlexLundinEducational <31811167+AlexLundinEducational@users.noreply.github.com>
2018-11-18 15:48:31 -0600

    localServer connected and able to query for booksearch

--- Example/dbconfig.js
+++ Example/dbconfig.js
@@ -69,16 +69,16 @@
  *****************************************************************************/
 
 module.exports = {
-  user          : process.env.NODE_ORACLEDB_USER || "hr",
+  user          : process.env.NODE_ORACLEDB_USER || "CS4347F18",
 
   // Instead of hard coding the password, consider prompting for it,
   // passing it in an environment variable via process.env, or using
   // External Authentication.
-  password      : process.env.NODE_ORACLEDB_PASSWORD || "welcome",
+  password      : process.env.NODE_ORACLEDB_PASSWORD || "a1",
 
   // For information on connection strings see:
   // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
-  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orclpdb",
+  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl",
 
   // Setting externalAuth is optional.  It defaults to false.  See:
   // https://oracle.github.io/node-oracledb/doc/api.html#extauth
